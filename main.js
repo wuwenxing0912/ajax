a6c711cff5fe34a53ac069639a6511eff122d343
wuwenxing <1428560819@qq.com>
2018-06-16 02:11:20 +0800

    Promise

--- main.js
+++ main.js
@@ -6,36 +6,28 @@ window.jQuery = function(nodeOrSelector) {
 }
 window.$ = window.jQuery
 
-window.jQuery.ajax = function({ url, method, body, successFn, failFn, headers }) {
-    //可将函数的参数改为obj
-    //let { url, method, body, successFn, failFn, headers } = obj  效果一致
-    //ES6解构赋值
+window.jQuery.ajax = function({ url, method, body, headers }) {
 
-    let request = new XMLHttpRequest()
-    request.open(method, url) // 配置request
-    for (let key in headers) {
-        let value = headers[key]
-        request.setRequestHeader(key, value)
-    }
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                successFn.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                failFn.call(undefined, request)
+    return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest()
+        request.open(method, url) // 配置request
+        for (let key in headers) {
+            let value = headers[key]
+            request.setRequestHeader(key, value)
+        }
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    reject.call(undefined, request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })
 }
 
-function f1(responseText) {
-    console.log(responseText)
-}
-
-function f2(responseText) {
-    console.log(responseText)
-}
 
 myButton.addEventListener('click', (e) => {
     window.jQuery.ajax({
@@ -44,15 +36,6 @@ myButton.addEventListener('click', (e) => {
         headers: {
             'content-type': 'application/x-www-form-urlencoded',
             'w': '18'
-        },
-        successFn: (responseText) => {
-            f1.call(undefined, responseText)
-            f2.call(undefined, responseText)
-        },
-        failFn: (request) => {
-            console.log(request)
-            console.log(request.status)
-            console.log(request.responseText)
         }
-    })
+    }).then((responseText) => { console.log(responseText) }, (request) => { console.log(request) })
 })
